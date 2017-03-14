33e80113c388f3823141fe625a986b224bda89fb
Carlos Quiroga <cquiro@gmail.com>
2017-03-14 16:11:08 -0300

    Write routes and provisional views for librarium

--- src/app/app.routes.js
+++ src/app/app.routes.js
@@ -14,11 +14,116 @@ angular.module('librarium').config([
           }
         }
       })
-      .state('home', {
-        url: '/',
+      // .state('home', {
+      //   url: '/',
+      //   views: {
+      //     main: {
+      //       templateUrl: '../app/components/home/home.html'
+      //     }
+      //   }
+      // })
+      .state('books', {
+        url: '/books',
         views: {
           main: {
-            templateUrl: '../app/components/home/home.html'
+            templateUrl: '../app/components/books/index.html'
+          }
+        }
+      })
+      .state('new_book', {
+        url: '/books/new',
+        views: {
+          main: {
+            templateUrl: '../app/components/books/new.html'
+          }
+        }
+      })
+      .state('book', {
+        url: '/books/:id',
+        views: {
+          main: {
+            templateUrl: '../app/components/books/show.html',
+            controller: 'idController'
+          },
+          'comments@book': {
+            templateUrl: '../app/components/comments/index.html'
+          },
+          'comments_new@book': {
+            templateUrl: '../app/components/comments/new.html'
+          }
+        }
+      })
+      .state('edit_book', {
+        url: '/books/:id/edit',
+        views: {
+          main: {
+            templateUrl: '../app/components/books/edit.html'
+          }
+        }
+      })
+      .state('edit_comment', {
+        url: '/comments/:id/edit',
+        views: {
+          main: {
+            templateUrl: '../app/components/comments/edit.html'
+          }
+        }
+      })
+      .state('signup', {
+        url: '/signup',
+        views: {
+          main: {
+            templateUrl: '../app/components/users/registration.html'
+          }
+        }
+      })
+      .state('login', {
+        url: '/login',
+        views: {
+          main: {
+            templateUrl: '../app/components/users/sessions_new.html'
+          }
+        }
+      })
+      .state('users', {
+        url: '/users',
+        views: {
+          main: {
+            templateUrl: '../app/components/users/index.html'
+          }
+        }
+      })
+      .state('user', {
+        url: '/users/:id',
+        views: {
+          main: {
+            templateUrl: '../app/components/users/show.html',
+            controller: 'idController'
+          }
+        }
+      })
+      .state('user.following', {
+        url: '/following',
+        templateUrl: '../app/components/users/following.html'
+      })
+      .state('user.followers', {
+        url: '/followers',
+        templateUrl: '../app/components/users/followers.html'
+      })
+      .state('user.favorite_books', {
+        url: '/favorite_books',
+        templateUrl: '../app/components/users/favorite_books.html'
+      })
+      .state('user.wish_list', {
+        url: '/wish_list',
+        templateUrl: '../app/components/users/wish_list.html'
+      })
+      .state('edit_user', {
+        url: '/users/:id/edit',
+        views: {
+          main: {
+            templateUrl: '../app/components/users/edit.html',
+            controller: 'idController'
           }
         }
       });
@@ -26,3 +131,9 @@ angular.module('librarium').config([
     $locationProvider.html5Mode(true);
   }
 ]);
+
+// Temporary controller to see the param id on books and users show pages.
+// It will be removed or replaced as new features are developed.
+angular.module('librarium').controller('idController', function ($stateParams, $scope) {
+  $scope.id = $stateParams.id;
+});

c4a91f2beea0d6491355c384636bfb783b7d4e0e
Carlos Quiroga <cquiro@gmail.com>
2017-03-21 13:34:38 -0300

    Fix indentation problem

--- src/app/directives.js
+++ src/app/directives.js
@@ -4,7 +4,7 @@ angular.module('librarium.directives', [])
       require: 'ngModel',
       link: function (scope, elem, attrs, ctrl) {
         const firstPassword = '#' + attrs.pwCheck;
-          $(elem).add(firstPassword).on('keyup', function () {
+        $(elem).add(firstPassword).on('keyup', function () {
           scope.$apply(function () {
             const v = elem.val() === $(firstPassword).val();
             ctrl.$setValidity('pwmatch', v);

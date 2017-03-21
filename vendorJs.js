7022d430300ad12407e8113655aab81e548ba5fd
Carlos Quiroga <cquiro@gmail.com>
2017-03-21 20:06:44 -0300

    Add angular-permission and angular-permission-ui to vendorJs

--- vendorJs.js
+++ vendorJs.js
@@ -4,5 +4,7 @@ module.exports = [
   'angular-translate/angular-translate.js',
   'lodash/lodash.js',
   'restangular/src/restangular.js',
-  'jquery/dist/jquery.min.js'
+  'jquery/dist/jquery.min.js',
+  'angular-permission/dist/angular-permission.min.js',
+  'angular-permission/dist/angular-permission-ui.min.js'
 ];

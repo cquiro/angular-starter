6f34565ac4cde22fd41c25aae34bc60b0c4dbf0a
Carlos Quiroga <cquiro@gmail.com>
2017-03-25 17:52:58 -0300

    Remove translations.controller and move functionality into app.controller.js

--- src/app/app.controller.js
+++ src/app/app.controller.js
@@ -1,11 +1,15 @@
 angular.module('librarium')
   .controller('ApplicationController',
-    ['AuthService', '$state', function (authService, $state) {
+    ['AuthService', '$state', '$translate', function (authService, $state, $translate) {
       const self = this;
 
       self.destroySession = function () {
         authService.logout();
         $state.go('root.books');
+      };
+
+      self.changeLanguage = function (langKey) {
+        $translate.use(langKey);
       };
     }]);
 
--- src/app/translations.controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('librarium')
-  .controller('TranslationController', function ($translate, $scope) {
-    $scope.changeLanguage = function (langKey) {
-      $translate.use(langKey);
-    };
-  });

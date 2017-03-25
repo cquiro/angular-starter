5b598ba311301fca752d72495d4c91b84826b5e3
Carlos Quiroga <cquiro@gmail.com>
2017-03-25 13:30:56 -0300

    Extract logic from app.permissions.js into role.service.js

--- src/app/app.permissions.js
+++ src/app/app.permissions.js
@@ -1,57 +1,21 @@
-angular.module('librarium').run(function (PermRoleStore, UserPersistence) {
+angular.module('librarium').run(function (PermRoleStore, IdentifyRole) {
   // logged in users
   PermRoleStore.defineRole('USER', function () {
-    let result = false;
-    const user = UserPersistence.getUserData();
-
-    if (user !== null) {
-      result = true;
-    } else {
-      result = false;
-    }
-
-    return result;
+    return IdentifyRole.isUser();
   });
 
   // logged out users
   PermRoleStore.defineRole('ANONYMOUS', function () {
-    let result = false;
-    const user = UserPersistence.getUserData();
-
-    if (user !== null) {
-      result = false;
-    } else {
-      result = true;
-    }
-
-    return result;
+    return IdentifyRole.isAnonymous();
   });
 
   // logged in users who are admins
   PermRoleStore.defineRole('ADMIN', function () {
-    let result = false;
-    const user = JSON.parse(UserPersistence.getUserData());
-
-    if (user !== null) {
-      result = user.admin;
-    } else {
-      result = false;
-    }
-
-    return result;
+    return IdentifyRole.isAdmin();
   });
 
   // logged in users who are not admins
   PermRoleStore.defineRole('BASIC', function () {
-    let result = false;
-    const user = JSON.parse(UserPersistence.getUserData());
-
-    if (user !== null) {
-      result = !user.admin;
-    } else {
-      result = false;
-    }
-
-    return result;
+    return IdentifyRole.isBasic();
   });
 });
